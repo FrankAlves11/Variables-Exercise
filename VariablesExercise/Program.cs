2d2188d06730cc2a05e08ffd36d03627e15feefe
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-26 21:05:25 -0700

    made changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare and initialize variables
+          
+            string name = "Frank";
+            int age = 22;
+            char middleInitial = 'S';
+            bool isOver18 = true;
+            double currentTemp = 99;
+            decimal currentPrice = 45;
+
+
+            // Writing the variables
+            
+            Console.WriteLine($"Hi, My name is {name} I am {age} years old.");
+            Console.WriteLine($"My middle name starts with a {middleInitial} it is {isOver18} that I am over 18.");
+            Console.WriteLine($"It is a cold day in Phoenix today with a temperature of {currentTemp} degrees fahrenheit.");
+            Console.WriteLine($"I got a great deal on a Scotty Cameron at a garage sale for just {currentPrice} buckaroons.");
+
+
+
+
+
         }
     }
 }
